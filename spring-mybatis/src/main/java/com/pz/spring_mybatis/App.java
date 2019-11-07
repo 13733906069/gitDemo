2e2ecd4ad1346583d573b17e0a1cd6d62e67d3cb
kirito <1657450156@qq.com>
2019-11-07 19:19:27 +0800

    提交项目

---
+++ spring-mybatis/src/main/java/com/pz/spring_mybatis/App.java
@@ -0,0 +1,13 @@
+package com.pz.spring_mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
